4d5aa43b2d0b0dccf632fbfa5e617b2dc5aceee2
OtavioFilipe <darksnack16@gmail.com>
2022-11-07 17:18:51 -0300

    fix

--- App.tsx
+++ App.tsx
@@ -4,20 +4,22 @@ import {
   Roboto_700Bold,
   useFonts,
 } from "@expo-google-fonts/roboto";
+import { StatusBar, View } from "react-native";
+
 import { NativeBaseProvider } from "native-base";
-import { StatusBar } from "react-native";
+import { THEME } from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
