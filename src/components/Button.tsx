9b44820604784a708b11f716cfa70837a1aeb709
OtavioFilipe <darksnack16@gmail.com>
2022-11-08 19:58:31 -0300

    create button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,10 +4,27 @@ type Props = IButtonProps & {
   title: string;
 };
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, variant, ...rest }: Props) {
   return (
-    <NativeBaseButton>
-      <Text>{title}</Text>
+    <NativeBaseButton
+      w="full"
+      h={14}
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
+      rounded="sm"
+      _pressed={{
+        bg: variant === "outline" ? "gray.500" : "green.500",
+      }}
+      {...rest}
+    >
+      <Text
+        color={variant === "outline" ? "green.500" : "white"}
+        fontFamily="heading"
+        fontSize="sm"
+      >
+        {title}
+      </Text>
     </NativeBaseButton>
   );
 }
